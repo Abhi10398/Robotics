f175b0874480f07893292dc1bef162468e1346a3
Abhi10398 <66339124+Abhi10398@users.noreply.github.com>
2020-06-03 06:33:48 +0530

    code uploaded

---
+++ Jacobian Matrix and Workspace Boundary Conditions/Example1/code/rot_z.m
@@ -0,0 +1,3 @@
+function r2 = rot_z(theta)
+    r2=[cos(theta) -sin(theta) 0 0; sin(theta) cos(theta) 0 0 ;0 0 1 0; 0 0 0 1];
+end
---
+++ Jacobian Matrix and Workspace Boundary Conditions/Example1/code/trans_x.m
@@ -0,0 +1,3 @@
+function t2 = trans_x(a)
+    t2=[1 0 0 a ; 0 1 0 0; 0 0 1 0; 0 0 0 1];
+end
